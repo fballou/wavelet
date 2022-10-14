9de255858453c77377379fc152742ce614bc85eb
fballou <fballou@ucsd.edu>
2022-10-13 21:38:35 -0700

    lab2

--- SearchEngine.java
+++ SearchEngine.java
@@ -9,24 +9,41 @@ public class SearchEngine implements URLHandler{
         if (url.getPath().equals("/")) {
             return stringz.toString();
         } 
-        else if (url.getPath().equals("/add")) {
-            stringz.add(url.getQuery());
-            System.out.println(url.getPath());
-            return String.format("Query added!");
-        } 
-        else {
+        if (url.getPath().contains("/add")) {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/search")) {
-                String[] parameters = url.getQuery().split("=");
+            String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
                     stringz.add(parameters[1]);
                     return parameters[1] + "query has been added";
+                }
+            
+            // stringz.add(url.getQuery());
+            // System.out.println(url.getPath());
+            // return String.format("Query added!");
+            return "404 Not Found!";
+        } 
+        else {
+            if (url.getPath().equals("/search")) {
+                ArrayList<String> temp = new ArrayList<>();
+                System.out.println("Path: +" + url.getPath());
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    for (int i = 0; i < stringz.size(); i++) {
+                        if (stringz.get(i).contains(parameters[1])) {
+                            temp.add(stringz.get(i));
+                        }
+                    }
+                    return temp.toString();
                 }
             }
             return "404 Not Found!";
         }
     }
 
+
+}
+
+class SearchEngineTest {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
@@ -35,7 +52,8 @@ public class SearchEngine implements URLHandler{
 
         int port = Integer.parseInt(args[0]);
 
-        Server.start(port, new Handler());
+        Server.start(port, new SearchEngine());
     }
+
 }
 
